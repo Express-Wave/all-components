37b8cfbd2fa002e54c0deb4badde22c67462db20
Oliver Abreu <oabreu@extron.com>
2024-08-20 13:35:52 -0400

    feat: makes sidebar content fade in after sidebar is full width

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -2,11 +2,12 @@
 	import CloseIcon from '$lib/icons/CloseIcon.svelte';
 	import LogoutIcon from '$lib/icons/LogoutIcon.svelte';
 	import MenuIcon from '$lib/icons/MenuIcon.svelte';
-	let height: string = '3.5rem';
 	import '../../app.css';
+	import { animationDuration } from '$lib/stores/animation.store';
+	import { onDestroy } from 'svelte';
 
+	let height: string = '3.5rem';
 	let closed: boolean = true;
-
 	export let width: string;
 
 	function toggleSidebar(event: Event) {
@@ -15,6 +16,30 @@
 		console.log('Sidebar closed?', closed);
 	}
 	let widthClass = `w-${width}`;
+
+	let fadeDuration: number;
+	let shouldFadeIn: boolean = false;
+
+	const unsubscribe = animationDuration.subscribe((value) => {
+		// fadeDuration = value;
+		fadeDuration = value - 200;
+	});
+
+	// Clean up subscription on component destroy
+	onDestroy(() => {
+		unsubscribe();
+	});
+
+	// Reactive statement to handle fade-in delay
+	$: {
+		if (!closed) {
+			setTimeout(() => {
+				shouldFadeIn = true;
+			}, fadeDuration);
+		} else {
+			shouldFadeIn = false;
+		}
+	}
 </script>
 
 <header
@@ -27,7 +52,6 @@
 	<button
 		class={`flex cursor-pointer min-h-[${height}] max-h-12 items-center border-b px-6 border-neutral-200 dark:border-neutral-700`}
 		on:click={toggleSidebar}
-		on:click={() => console.log('Button clicked')}
 	>
 		{#if closed}
 			<MenuIcon />
@@ -40,18 +64,23 @@
 	</button>
 	<div class="min-h-[100%] max-h-90vh overflow-none py-5 px-6">
 		{#if !closed}
-			<span>Here's some Menu stuff!</span>
+			<div
+				class={`ease-in-out transition-opacity ${shouldFadeIn ? 'opacity-100' : 'opacity-0'}`}
+				style={`transition-duration: ${fadeDuration}ms;`}
+			>
+				<span>Here's some Menu stuff!</span>
+			</div>
 		{/if}
 	</div>
 	<a
 		target="_self"
 		href="/"
 		role="menuitem"
-		class="block -mt-1 flex flex-col border-t py-5 px-6 border-nuetral-200 dark:border-nuetral-700"
+		class="block -mt-1 flex flex-col border-t py-5 px-6 border-neutral-200 dark:border-neutral-700"
 		style="position: sticky; bottom: 0;"
 	>
 		<div
-			class="cursor-pointer flex space-x-3 items-center outline-none focus-visible:ring-1-muted focus-visible:z-10 group py-1 font-normal border-nuetral-200 dark:border-nuetral-700"
+			class="cursor-pointer flex space-x-3 items-center outline-none focus-visible:ring-1-muted focus-visible:z-10 group py-1 font-normal border-neutral-200 dark:border-neutral-700"
 		>
 			<div class="transition truncate text-sm group-hover min-w-fit">
 				<LogoutIcon />
