6ad05c91379b16942d65a39d0627576bb8824982
Oliver Abreu <oliverabreu@icloud.com>
2024-12-07 21:26:36 -0500

    💬 Updates site text

--- src/routes/plan-your-visit/+page.svelte
+++ src/routes/plan-your-visit/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Input, Label, Helper, Textarea } from 'flowbite-svelte';
+	import { Input, Label, Helper, Textarea, Button } from 'flowbite-svelte';
 
 	// FORMSUBMIT, NUCLEUS GIVING, WEB3FORMS,
 	// ASTRO: https://preview.tailus.io/astrolus-free/
@@ -29,7 +29,7 @@
 		},
 		{
 			question: "What's a typical church service like?",
-			answer: `Our church services are a little over an hour long and include congregational singing, Bible reading, special music, a relevant message from the Bible, and a time of response to the message.`,
+			answer: `Our services last just over an hour and include congregational singing, Bible reading, special music, a practical message from the Bible, and a time to respond to the message`,
 			icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               
 	<path fill="currentColor" fill-rule="evenodd" d="M12 3a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1.5l7.6 5.7A1 1 0 0 1 20 16h-1v3h2a1 1 0 1 1 0 2h-7v-3a2 2 0 1 0-4 0v3H3a1 1 0 1 1 0-2h2v-3H4a1 1 0 0 1-.6-1.8L11 8.5V7h-1a1 1 0 0 1 0-2h1V4a1 1 0 0 1 1-1" clip-rule="evenodd" />
@@ -37,14 +37,14 @@
 		},
 		{
 			question: 'Are all your services the same?',
-			answer: `The focus of our Sunday 10:00 AM service is small-group Bible study, while our Sunday 11:00 AM, and Sunday 6:30 PM services are two distinct worship services with unique Bible preaching for each service. Our Wednesday 7:00 PM service is a large-group Bible study, with a time for communal prayer.`,
+			answer: `Our Sunday 10:00 AM service focuses on small-group Bible study. The 11:00 AM and 6:30 PM Sunday services are distinct worship services, each featuring unique Bible preaching. On Wednesdays at 7:00 PM, we gather for a large-group Bible study and a time of communal prayer.`,
 			icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path fill="currentColor" d="M13 3C9.23 3 6.19 5.95 6 9.66l-1.92 2.53c-.24.31 0 .81.42.81H6v3c0 1.11.89 2 2 2h1v3h7v-4.69c2.37-1.12 4-3.51 4-6.31c0-3.86-3.12-7-7-7m1 11h-2v-2h2zm1.75-5.19c-.29.4-.66.69-1.11.93c-.25.16-.42.33-.51.52c-.09.18-.13.43-.13.74h-2c0-.5.11-.92.31-1.18c.19-.27.54-.57 1.05-.91c.26-.16.47-.36.61-.59c.16-.23.23-.5.23-.82c0-.3-.08-.56-.26-.75c-.18-.18-.44-.28-.75-.28a1 1 0 0 0-.66.23c-.18.16-.27.39-.28.69h-1.93l-.01-.03c-.01-.79.25-1.36.77-1.77c.54-.39 1.24-.59 2.11-.59c.93 0 1.66.23 2.19.68c.54.45.81 1.06.81 1.82c0 .5-.15.91-.44 1.31" />
             </svg>`
 		},
 		{
 			question: 'Am I expected to give an offering?',
-			answer: `No, as a visitor, we hope that our service will be a gift to you. We do believe that the Bible teaches that it is the responsibilty of a church to financially support itself; therefore, church members and regular attenders are encouraged to give.`,
+			answer: `No, as our guest, we want our service to be a gift to you. While we believe the Bible teaches that the church should support itself financially, giving is a responsibility for our members and regular attenders—not our visitors.`,
 			icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 				<path fill="currentColor" d="M12 12.5a3.5 3.5 0 1 0 0 7a3.5 3.5 0 0 0 0-7M10.5 16a1.5 1.5 0 1 1 3 0a1.5 1.5 0 0 1-3 0" />
 				<path fill="currentColor" d="M17.526 5.116L14.347.659L2.658 9.997L2.01 9.99V10H1.5v12h21V10h-.962l-1.914-5.599zM19.425 10H9.397l7.469-2.546l1.522-.487zM15.55 5.79L7.84 8.418l6.106-4.878zM3.5 18.169v-4.34A3 3 0 0 0 5.33 12h13.34a3 3 0 0 0 1.83 1.83v4.34A3 3 0 0 0 18.67 20H5.332A3.01 3.01 0 0 0 3.5 18.169" />
@@ -52,7 +52,7 @@
 		},
 		{
 			question: 'How should I dress?',
-			answer: `Wear what you feel is appropriate! There is no dress code for visitors or members at EBC. Our ministry leaders and many of our church family dress in more traditional “Sunday” dress; however, our main goal is that you would feel welcome and comfortable on your visit here!`,
+			answer: `Come as you are! There’s no dress code for visitors or members at EBC. While many of our ministry leaders and church family choose more traditional ‘Sunday’ attire, our priority is that you feel comfortable and welcome during your visit.`,
 			icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
 				<path fill="currentColor" d="M133.3 33.41L77.89 47.25L34.6 148.3l33.29 22.2l27.46-54.9l17.05 4.9l-15.07 150.1H245.2l9.2-87.9l.9-8.1h4.5l-5.4-54.1l17.1-4.9l27.4 54.9l33.3-22.2l-43.3-101.05l-55.4-13.84c-5.5 3.87-12.2 6.21-19.5 7.95c-9.4 2.21-20 3.24-30.6 3.24s-21.2-1.03-30.6-3.24c-7.3-1.74-14-4.07-19.5-7.95M271.5 192.6l-1.5 14h178.8l-1.5-14zm-3.4 32l-26.7 254h62.7l46.5-216.9h17.6l46.5 216.9h62.7l-26.7-254z" />
 			</svg>`
@@ -97,7 +97,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="mb-20 text-gray-600 dark:text-gray-300 max-w-8xl" id="faqs">
+		<div class="my-20 text-gray-600 dark:text-gray-300 max-w-8xl" id="faqs">
 			<!-- <div class="px-6 mx-auto max-w-7xl md:px-12 xl:px-6">
 				<div class="px-6 mb-20 space-y-4 md:px-0">
 					<h2 class="text-2xl font-bold text-center text-gray-800 dark:text-white md:text-4xl">
@@ -188,7 +188,7 @@
 			</div>
 		</div>
 	</section>
-	<section class="my-20">
+	<section class="p-4 mx-auto mb-20 max-w-2xl">
 		<div class="flex-col items-center px-6 mx-auto w-full max-w-7xl md:px-12 xl:px-6">
 			<div class="relative flex-col items-center pt-36 ml-auto">
 				<div class="flex-col items-center mx-auto w-full text-center md:w-1/2 lg:w-2/3">
@@ -196,7 +196,7 @@
 				Reach Out Directly
 			</h2>
 			<h3 class="mt-8 mb-20 text-lg text-gray-700">
-				Visiting a church can be an awkard and intimadting expereince; we want to make you feel at ease about anything not listed here and give you a place to reach out before arriving on campus.  
+				Visiting a new church can feel intimidating, but we’re here to help you feel at home. Use the form below to ask us anything!  
 			</h3>
 				</div>
 			</div>
@@ -205,34 +205,35 @@
 			<input type="hidden" name="access_key" value={ACCESS_KEY} />
 			<div class="mb-5">
 				<Label class="space-y-2">
-					<span>Your Name*</span>
+					<span class="capitalize">Your Name*</span>
 					<Input type="name" placeholder="First Last" size="lg" required />
 				</Label>
 			</div>
 			<div class="mb-5">
 				<Label class="space-y-2">
-					<span>Your email address*</span>
+					<span class="capitalize">Your email address*</span>
 					<Input type="email" placeholder="name@provider.com" size="lg" required>
 						<!-- <EnvelopeSolid slot="right" class="w-5 h-5" /> -->
 					</Input>
-					<Helper class="text-sm text-gray-600">
-						We’ll never share your details. Read our <a
-							href="/"
-							class="font-medium text-theme-600 hover:underline dark:text-theme-500"
-						>
-							Privacy Policy
-						</a>
-						.
-					</Helper>
+					
 				</Label>
 			</div>
 			<div class="mb-5">
-				<!-- <Label class="space-y-2">
-					<span>Your question here*</span>
-					<Input class="h-40" clearable value="Your question here" />
-				</Label> -->
-				<Label for="textarea-id" class="mb-2">Your message*</Label>
-				<Textarea id="textarea-id" placeholder="Your message" rows="4" name="message" required />
+				<Label for="textarea-id" class="mb-2 capitalize">Your message/question*</Label>
+				<!-- <Textarea id="textarea-id" placeholder="Your message/question" rows="4" name="message" class="p-2" required /> -->
+				<Textarea class="mb-4" id="textarea-id" name="message" rows="4" placeholder="Do you have visitor parking?">
+					<div slot="footer" class="flex justify-between items-center">
+						<Helper class="text-sm text-gray-600">
+							We’ll never share your details. Read our <a
+								href="/legal#privacy-policy"
+								class="font-medium text-theme-600 hover:underline dark:text-theme-500"
+							>
+								Privacy Policy
+							</a>
+							.
+						</Helper>
+					</div>
+				</Textarea>
 			</div>
 			<input type="checkbox" name="botcheck" class="hidden" style="display: none;" />
 			<button
