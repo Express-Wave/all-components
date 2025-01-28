089d19271bc5208495e66e2fa5b5505e671fcb0f
Express-Wave <ExpressWave.net@gmail.com>
2025-01-28 14:20:14 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import '../global.css';
     import Footer from '$lib/components/Footer.svelte';
+    import Header from '$lib/components/Header.svelte';
 </script>
 
+
+<Header />
 <main>
     <!-- This slot will render the content of the specific page -->
     <slot />
